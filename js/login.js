d0440f4dc03d62172e0420e73b657e913bc1be28
mamun700263 <mamun700263@gmail.com>
2024-09-04 19:58:20 +0600

    allah jane

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
-// const baseUrl = "https://online-school-1wkk.onrender.com";
-const baseUrl = "http://127.0.0.1:8000";
+const baseUrl = "https://online-school-1wkk.onrender.com";
+// const baseUrl = "http://127.0.0.1:8000";
 const loginApiUrl = `${baseUrl}/accounts/login/`;
 
 document.getElementById('login-form').addEventListener('submit', submitLoginForm);
